8e4a019369161dbf9ee51585c0f97742cc26800e
Guillermo Facundo Colunga <colunga91@gmail.com>
2017-12-13 21:49:07 +0100

    Complete refactor of the project. Started the Swift project for java. A Fast framework to build java applications.

---
+++ stdlib/Foundation/XMLParser.java
@@ -0,0 +1,31 @@
+/*
+ * This source file is part of the Swift open source project.
+ *
+ * Copyright (c) 2017 Guillermo Facundo Colunga and the Swift project authors.
+ * Licensed under GNU General Public License v3.0.
+ *
+ * See /LICENSE for license information.
+ * 
+ */
+package Foundation;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * Instance of XMLParser
+ * 
+ * @author Guillermo Facundo Colunga
+ */
+public class XMLParser implements Serializable {
+
+	private static final long serialVersionUID = 1939036191695213367L;
+	
+	private NSFile _file;
+	
+	public XMLParser(NSFile file) {
+		this._file = file;
+	}
+	
+	// TO DO....
+}
