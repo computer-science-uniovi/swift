02395b1bd0aa4fe27b2985bd06a649f1cdb53833
Guillermo Facundo Colunga <colunga91@gmail.com>
2017-12-13 23:38:38 +0100

    New tests upload

---
+++ test/ci/DemoTest.java
@@ -0,0 +1,14 @@
+package ci;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DemoTest {
+
+	@Test void test() {
+		assertEquals( true, true );
+		System.out.println("Congrats, tests running properly!");
+	}
+
+}
