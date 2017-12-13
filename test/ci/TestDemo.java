92546f5164c0c579f416d4e7de37c4a05f342b7a
Guillermo Facundo Colunga <colunga91@gmail.com>
2017-12-13 23:53:48 +0100

    test updated

---
+++ test/ci/TestDemo.java
@@ -0,0 +1,14 @@
+package ci;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDemo {
+
+	@Test public void test() {
+		assertEquals( true, true );
+		System.out.println( "Test passed!" );
+	}
+
+}
