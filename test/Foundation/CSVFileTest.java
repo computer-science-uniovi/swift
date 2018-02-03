d08c3c22a956696b868d8482d14b28b80bc0d187
Willy <ZenMaster91@users.noreply.github.com>
2018-02-03 21:17:09 +0100

    Revert "Improved the beta version to a stable one."

--- test/Foundation/CSVFileTest.java
+++ test/Foundation/CSVFileTest.java
@@ -10,37 +10,40 @@ import org.junit.Test;
 
 public class CSVFileTest {
 
-	private CSVFile<String, String> file;
+    private CSVFile file;
 
-	@Before public void setUp() {
-		file = new CSVFile<String, String>( new URL( "noname.csv" ) );
+    @Before
+    public void setUp() {
+	file = new CSVFile(new URL("noname.csv"));
+    }
+
+    @After
+    public void tearDown() {
+	try {
+
+	    File file = new File("noname.csv");
+
+	    if (file.delete()) {
+		System.out.println(file.getName() + " is deleted!");
+	    } else {
+		System.out.println("Delete operation is failed.");
+	    }
+	} catch (Exception e) {
+	    e.printStackTrace();
 	}
+    }
 
-	@After public void tearDown() {
-		try {
+    @Test
+    public void test() {
+	String[] data = { "first", "second", "third" };
+	String[] data2 = { "ss", "aa", "22" };
+	file.getContent().put("1", data);
+	file.getContent().put("2", data2);
+	file.setSeparator(",");
+	file.save();
 
-			File file = new File( "noname.csv" );
-
-			if (file.delete()) {
-				System.out.println( file.getName() + " is deleted!" );
-			} else {
-				System.out.println( "Delete operation is failed." );
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Test public void test() {
-		String[] data = { "first", "second", "third" };
-		String[] data2 = { "ss", "aa", "22" };
-		file.getContent().put( "1", data );
-		file.getContent().put( "2", data2 );
-		file.setSeparator( "," );
-		file.save();
-
-		file = CSVFile.of( new URL( "noname.csv" ), "," );
-		assertArrayEquals( data, file.getContent().get( "1" ) );
-	}
+	file = CSVFile.of(new URL("noname.csv"), ",");
+	assertArrayEquals(data, file.getContent().get("1"));
+    }
 
 }
